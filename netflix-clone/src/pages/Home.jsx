ba3696e515292d8ffe70bf82ce230fb5ffe0186f
Gabriele Castro <gabrielerayane2001@gmail.com>
2021-12-29 10:14:31 -0300

    Cria page Home

--- netflix-clone/src/pages/Home.jsx
+++ netflix-clone/src/pages/Home.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import NavBar from '../components/NavBar';
 
+/* import { AiOutlineInfoCircle} from 'react-icons/fa'; */
+
 class Home extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <main>
+          <div class="main-movie">
+            <div class="container-main-movie">
+              <h3 class="title">Perdidos no Espaço</h3>
+              <p class="sinopse">Após um pouso forçado em um planeta desconhecido, a família Robinson tenta sobreviver aos perigos que encontra neste novo mundo.</p>
+              <div class="buttons">
+                <button class="button">
+                  Assistir agora
+                </button>
+                <button class="button">
+                  Mais Informações
+                </button>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
